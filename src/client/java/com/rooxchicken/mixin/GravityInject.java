70b267bb83ee05ba12f6ae52fef485076b291c25
TurboJax <turbojax07@gmail.com>
2026-04-10 17:34:57 -0400

    Fixing GravityInject

--- src/client/java/com/rooxchicken/mixin/GravityInject.java
+++ src/client/java/com/rooxchicken/mixin/GravityInject.java
@@ -1,14 +1,16 @@
 package com.rooxchicken.mixin;
 
 import com.rooxchicken.client.PsychisModClient;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin(PlayerEntity.class)
+@Mixin(LivingEntity.class)
 public class GravityInject {
-    @ModifyVariable(method = "travel(Lnet/minecraft/util/math/Vec3d;)V", at = @At("STORE"), ordinal = 0)
+    PlayerEntity player;
+    @ModifyVariable(method = "travelMidAir(Lnet/minecraft/util/math/Vec3d;)V", at = @At("STORE"), ordinal = 0)
     private double travelInject(double d) {
         return PsychisModClient.playerAbility == 5 ? 0.1D : d;
     }
